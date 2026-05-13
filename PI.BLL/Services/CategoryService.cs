3cbce1e3b3adc15bacafea3668e73e262b81729d
ArtemBereznii <artem.berezniy8@gmail.com>
2026-05-13 15:03:06 +0300

    feat(BLL): implement CategoryService

--- PI.BLL/Services/CategoryService.cs
+++ PI.BLL/Services/CategoryService.cs
@@ -1,10 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AutoMapper;
+using PI.BLL.DTOs.Catalog;
+using PI.BLL.Interfaces;
+using PI.DAL.Interfaces;
+using PI.DAL.Entities.Catalog;
 
-namespace PI.BLL.Services
+namespace PI.BLL.Services;
+
+public class CategoryService : ICategoryService
 {
-    internal class CategoryService
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public CategoryService(IUnitOfWork unitOfWork, IMapper mapper)
     {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<Guid> CreateAsync(CreateCategoryRequest request)
+    {
+        var category = Category.Create(request.Name, request.Description);
+
+        await _unitOfWork.Categories.AddAsync(category);
+        await _unitOfWork.CompleteAsync();
+
+        return category.Id;
+    }
+
+    public async Task UpdateAsync(UpdateCategoryRequest request)
+    {
+        var category = await _unitOfWork.Categories.GetByIDAsync(request.Id)
+            ?? throw new KeyNotFoundException($"Category with ID {request.Id} was not found.");
+
+        bool hasChanges = false;
+
+        if (request.Name != category.Name)
+        {
+            category.UpdateName(request.Name);
+            hasChanges = true;
+        }
+
+        if (request.Description != category.Description)
+        {
+            category.UpdateDescription(request.Description);
+            hasChanges = true;
+        }
+
+        if (!hasChanges) return;
+
+        _unitOfWork.Categories.Update(category);
+        await _unitOfWork.CompleteAsync();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var category = await _unitOfWork.Categories.GetByIDAsync(id)
+            ?? throw new KeyNotFoundException($"Category with ID {id} was not found.");
+
+        _unitOfWork.Categories.Delete(category);
+        await _unitOfWork.CompleteAsync();
+    }
+
+    public async Task<CategoryResponse?> GetByIdAsync(Guid id)
+    {
+        var category = await _unitOfWork.Categories.GetByIDAsync(id)
+            ?? throw new KeyNotFoundException($"Category with ID {id} was not found.");
+
+        return _mapper.Map<CategoryResponse>(category);
+    }
+
+    public async Task<List<CategoryResponse>> GetAllAsync()
+    {
+        var categories = await _unitOfWork.Categories.GetAllAsync();
+        return _mapper.Map<List<CategoryResponse>>(categories);
     }
 }
