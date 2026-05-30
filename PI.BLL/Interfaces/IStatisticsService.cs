1d673e06744f8ab23627c09073211fa2522379cb
ArtemBereznii <artem.berezniy8@gmail.com>
2026-05-30 18:04:01 +0300

    feat(BLL): add IStatisticsService interface

---
+++ PI.BLL/Interfaces/IStatisticsService.cs
@@ -0,0 +1,16 @@
+﻿using PI.BLL.DTOs.Statistic;
+
+namespace PI.BLL.Interfaces;
+
+public interface IStatisticsService
+{
+    Task<SummaryResponse> GetSummaryAsync(DateTime startDate, DateTime endDate, CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<TopProductResponse>> GetTopSellingProductsAsync(int limit, CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<CategoryRevenueResponse>> GetRevenueByCategoryAsync(CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<LowStockResponse>> GetLowStockProductsAsync(int threshold, CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<TopUserResponse>> GetTopUsersAsync(int limit, CancellationToken cancellationToken = default);
+}
