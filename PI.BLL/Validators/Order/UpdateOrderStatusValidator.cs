a088ea22111d1e97de6064db038c9b55cbeebb89
temandro <9128753@stud.kai.edu.ua>
2026-06-01 21:46:20 +0300

    feat(BLL): add UpdateOrderStatusValidator (#168)

---
+++ PI.BLL/Validators/Order/UpdateOrderStatusValidator.cs
@@ -0,0 +1,20 @@
+using FluentValidation;
+using PI.BLL.DTOs.Orders;
+using PI.DAL.Enums;
+
+namespace PI.BLL.Validators.Order;
+
+public class UpdateOrderStatusValidator : AbstractValidator<UpdateOrderStatusRequest>
+{
+    public UpdateOrderStatusValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Order ID is required.");
+
+        RuleFor(x => x.Status)
+            .NotEmpty().WithMessage("Status cannot be empty if provided.")
+            .IsEnumName(typeof(OrderStatus), caseSensitive: false)
+            .WithMessage("Invalid Status. Allowed: New, Paid, Delivered, Cancelled.")
+            .When(x => x.Status != null);
+    }
+}
