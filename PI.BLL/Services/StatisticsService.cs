4b84819d8e936eb5d93611a552d5df3e47d06590
ArtemBereznii <artem.berezniy8@gmail.com>
2026-06-03 22:52:22 +0300

    fix(BLL): delete unnecessary using

--- PI.BLL/Services/StatisticsService.cs
+++ PI.BLL/Services/StatisticsService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using PI.BLL.DTOs.Statistic;
 using PI.BLL.Interfaces;
 using PI.DAL.Interfaces;
