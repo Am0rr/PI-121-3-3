d520960c5852df9d43581cfdd4fb18dd1e2b55c1
Am0rr <itsaizel468@gmail.com>
2026-06-03 16:21:04 +0300

    feat(PL): implement AuthController (#250)

---
+++ PI.PL/Controllers/AuthController.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PI.BLL.DTOs.Identity;
+using PI.BLL.Interfaces;
+
+namespace PI.PL.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[AllowAnonymous]
+public class AuthController : ControllerBase
+{
+    private readonly IUserService _userService;
+    private readonly IAuthService _authService;
+
+    public AuthController(IUserService userService, IAuthService authService)
+    {
+        _userService = userService;
+        _authService = authService;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<UserResponse>> RegisterAsync([FromBody] CreateUserRequest request, CancellationToken cancellationToken)
+    {
+        var user = await _userService.CreateAsync(request, cancellationToken);
+
+        return CreatedAtAction(nameof(RegisterAsync), new { id = user.Id }, user);
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<AuthResponse>> LoginAsync([FromBody] LoginRequest request, CancellationToken cancellationToken)
+    {
+        var user = await _authService.LoginAsync(request, cancellationToken);
+
+        return Ok(user);
+    }
+
+    [HttpPost("refresh")]
+    public async Task<ActionResult<AuthResponse>> RefreshAsync([FromBody] string refreshToken, CancellationToken cancellationToken)
+    {
+        var token = await _authService.RefreshAsync(refreshToken, cancellationToken);
+
+        return Ok(token);
+    }
+
+    [HttpPost("revoke")]
+    public async Task<IActionResult> RevokeAsync([FromBody] string refreshToken, CancellationToken cancellationToken)
+    {
+        await _authService.RevokeAsync(refreshToken, cancellationToken);
+
+        return NoContent();
+    }
+}
