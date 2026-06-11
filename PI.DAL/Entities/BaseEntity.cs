0b4620e3e3080eb99144cc4f0c08f4aadab8cd23
Am0rr <itsaizel468@gmail.com>
2026-06-11 14:06:48 +0300

    refactor(DAL): remove unused UpdatedAt from BaseEntity

--- PI.DAL/Entities/BaseEntity.cs
+++ PI.DAL/Entities/BaseEntity.cs
@@ -4,5 +4,4 @@ public abstract class BaseEntity
 {
     public Guid Id { get; private set; } = Guid.NewGuid();
     public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
-    public DateTime? UpdatedAt { get; private set; }
 }
